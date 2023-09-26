0c8c793cde2fb6781083c4a8e46dc8f39ba2710d
Tino Kühnel <Tino.Kuehnel@klueber.com>
2023-09-26 08:14:57 +0200

    init

---
+++ ColorDropdown/Client/Shared/Components/ColorIndicator.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ColorDropdown.Client.Shared.Components
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public partial class ColorIndicators : Attribute { }
+    [AttributeUsage(AttributeTargets.Field)]
+    public partial class ColorIndicatorExecutions : Attribute { }
+
+    public enum ColorIndicator
+    {
+        [ColorIndicatorExecutions]
+        [ColorIndicators]
+        [Display(ResourceType = typeof(ColorIndicatorResource), Name = "Green")]
+        Green = 0,
+        [ColorIndicators]
+        [Display(ResourceType = typeof(ColorIndicatorResource), Name = "Yellow")]
+        Yellow = 1,
+        [ColorIndicatorExecutions]
+        [ColorIndicators]
+        [Display(ResourceType = typeof(ColorIndicatorResource), Name = "Red")]
+        Red = 2,
+        [ColorIndicators]
+        [Display(ResourceType = typeof(ColorIndicatorResource), Name = "Gray")]
+        Gray = 3
+    }
+}
---
+++ ColorDropdown/Client/Shared/Components/EnumExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace ColorDropdown.Client.Shared.Components
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Gets the display name.
+        /// </summary>
+        /// <param name="enumValue">The enum value.</param>
+        /// <returns>Display Name Value</returns>
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()?
+                .GetName() ?? Regex.Replace(enumValue.ToString(), "(?<=[a-z])([A-Z])", " $1", RegexOptions.Compiled).Trim();
+        }
+    }
+}
